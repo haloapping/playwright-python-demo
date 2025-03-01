3aa19306dc37ce8d5fc07056df90b1c940121cc5
haloapping <alfiyanto.kondolele@gmail.com>
2025-03-01 17:31:50 +0700

    chore: rename

---
+++ locator/selector.py
@@ -0,0 +1,28 @@
+# Home page
+MENU_TOGGLE = "#menu-toggle"
+LOGIN_MENU_TEXT = "#sidebar-wrapper > ul > li:nth-child(4) > a"
+
+# Login page
+USERNAME_TEXT = "#login > div > div > div.col-sm-offset-3.col-sm-6 > form > div.alert.alert-info > div:nth-child(1) > div > div > input"
+USERNAME_INPUT_TEXT = "#txt-username"
+PASSWORD_INPUT_TEXT = "#txt-password"
+PASSWORD_TEXT = "#login > div > div > div.col-sm-offset-3.col-sm-6 > form > div.alert.alert-info > div:nth-child(2) > div > div > input:nth-child(2)"
+LOGIN_BTN = "#btn-login"
+
+# Appointment page
+MAKE_APPOINTMENT_BTN = "#btn-make-appointment"
+FACILITY_DROPDOWNLIST = "#combo_facility"
+APPLY_FOR_HOSTPITAL_READMISSION_CHECKBOX = "#chk_hospotal_readmission"
+HEALTHCARE_PROGRAM_MEDICARE_RB = "#radio_program_medicare"
+HEALTHCARE_PROGRAM_MEDICAID_RB = "#radio_program_medicaid"
+HEALTHCARE_PROGRAM_NONE_RB = "#radio_program_none"
+VISIT_DATE_CALENDAR = "#txt_visit_date"
+COMMENT_INPUT_TEXT = "#txt_comment"
+BOOK_APPOINTMENT_BTN = "#btn-book-appointment"
+
+# Appointment Confirmation
+FACILITY_TEXT = "//*[@id='facility']"
+APPLY_FOR_HOSTPITAL_READMISSION_TEXT = "#hospital_readmission"
+HEALTHCARE_PROGRAM_TEXT = "#program"
+VISIT_DATE_TEXT = "#visit_date"
+COMMENT_TEXT = "#comment"
