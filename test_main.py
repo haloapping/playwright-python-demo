fef81873de08a1b2fcfe55edfd8907a03a502f34
haloapping <alfiyanto.kondolele@gmail.com>
2025-10-25 17:22:01 +0700

    fix: simplify code structure

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ from openpyxl import Workbook, load_workbook
 from playwright.sync_api import sync_playwright
 
 import config
-from selector import select
+import locator
 
 
 def random_date(start_date, end_date):
@@ -38,39 +38,48 @@ def test_demo():
 
         page = context.new_page()
         page.set_viewport_size(
-            {"width": config.VIEW_PORT_WIDTH_SIZE, "height": config.VIEW_PORT_HEIGHT_SIZE}
+            {
+                "width": config.VIEW_PORT_WIDTH_SIZE,
+                "height": config.VIEW_PORT_HEIGHT_SIZE,
+            }
         )
         page.goto(config.URL)
 
         # Homepage
-        page.click(select.MENU_TOGGLE)
+        page.click(locator.MENU_TOGGLE)
         page.screenshot(path="screenshot/001-homepage.png", full_page=True)
-        page.click(select.LOGIN_MENU_TEXT)
+        page.click(locator.LOGIN_MENU_TEXT)
 
         # Login page
-        username = page.input_value(select.USERNAME_TEXT)
-        password = page.input_value(select.PASSWORD_TEXT)
-        page.fill(select.USERNAME_INPUT_TEXT, username)
-        page.fill(select.PASSWORD_INPUT_TEXT, password)
+        username = page.input_value(locator.USERNAME_TEXT)
+        password = page.input_value(locator.PASSWORD_TEXT)
+        page.fill(locator.USERNAME_INPUT_TEXT, username)
+        page.fill(locator.PASSWORD_INPUT_TEXT, password)
         page.screenshot(path="screenshot/002-loginpage.png", full_page=True)
-        page.click(select.LOGIN_BTN)
+        page.click(locator.LOGIN_BTN)
 
         # Appointment page
-        page.select_option(select.FACILITY_DROPDOWNLIST, value="Seoul CURA Healthcare Center")
-        page.click(select.APPLY_FOR_HOSTPITAL_READMISSION_CHECKBOX)
-        page.click(select.HEALTHCARE_PROGRAM_MEDICAID_RB)
+        page.select_option(
+            locator.FACILITY_DROPDOWNLIST, value="Seoul CURA Healthcare Center"
+        )
+        page.click(locator.APPLY_FOR_HOSTPITAL_READMISSION_CHECKBOX)
+        page.click(locator.HEALTHCARE_PROGRAM_MEDICAID_RB)
         page.type(
-            select.VISIT_DATE_CALENDAR,
+            locator.VISIT_DATE_CALENDAR,
             random_date(pendulum.date(2020, 1, 1), pendulum.date(2025, 12, 31)),
         )
         page.keyboard.press("Tab")
         page.fill(
-            select.COMMENT_INPUT_TEXT,
-            random.choice(["Apping Ganteng 😉", "Alek Sayang Ibu ❤️", "Tangan Yoga bau rokok 🚬"]),
+            locator.COMMENT_INPUT_TEXT,
+            random.choice(
+                ["Apping Ganteng 😉", "Alek Sayang Ibu ❤️", "Tangan Yoga bau rokok 🚬"]
+            ),
         )
         page.screenshot(path="screenshot/003-appointment-form.png", full_page=True)
-        page.click(select.BOOK_APPOINTMENT_BTN)
-        page.screenshot(path="screenshot/004-appointment-confirmation.png", full_page=True)
+        page.click(locator.BOOK_APPOINTMENT_BTN)
+        page.screenshot(
+            path="screenshot/004-appointment-confirmation.png", full_page=True
+        )
 
         # Save appointment to excel file
         if not os.path.exists("appointment.xlsx"):
@@ -89,11 +98,13 @@ def test_demo():
             ws = wb["appointment"]
 
         last_row = ws.max_row + 1
-        ws["A" + str(last_row)] = page.inner_text(select.FACILITY_TEXT)
-        ws["B" + str(last_row)] = page.inner_text(select.APPLY_FOR_HOSTPITAL_READMISSION_TEXT)
-        ws["C" + str(last_row)] = page.inner_text(select.HEALTHCARE_PROGRAM_TEXT)
-        ws["D" + str(last_row)] = page.inner_text(select.VISIT_DATE_TEXT)
-        ws["E" + str(last_row)] = page.inner_text(select.COMMENT_TEXT)
+        ws["A" + str(last_row)] = page.inner_text(locator.FACILITY_TEXT)
+        ws["B" + str(last_row)] = page.inner_text(
+            locator.APPLY_FOR_HOSTPITAL_READMISSION_TEXT
+        )
+        ws["C" + str(last_row)] = page.inner_text(locator.HEALTHCARE_PROGRAM_TEXT)
+        ws["D" + str(last_row)] = page.inner_text(locator.VISIT_DATE_TEXT)
+        ws["E" + str(last_row)] = page.inner_text(locator.COMMENT_TEXT)
         ws["F" + str(last_row)] = "Appointment Success"
         wb.save("appointment.xlsx")
         wb.close()
