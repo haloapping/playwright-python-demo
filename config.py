5f76783d100380c89ec7e5e1ad1759124cfe8811
haloapping <alfiyanto.kondolele@gmail.com>
2025-10-25 18:29:19 +0700

    feat: add default config database

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 URL = "https://katalon-demo-cura.herokuapp.com/"
-HEADLESS = False
+HEADLESS = True
 SLOW_MO = 2000
 VIDEO_WIDTH_SIZE = 1920
 VIDEO_HEIGHT_SIZE = 1080
 VIEW_PORT_WIDTH_SIZE = 1920
 VIEW_PORT_HEIGHT_SIZE = 1080
+
+# DATABASE
+HOST="localhost"
+DBNAME="playwrightdemo"
+USER="postgres"
+PASSWORD="postgres"
