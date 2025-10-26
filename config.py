a37c821e3540f065bc8984042fb2392cad4b5da2
haloapping <alfiyanto.kondolele@gmail.com>
2025-10-26 14:31:41 +0700

    chore: run formatter

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 URL = "https://katalon-demo-cura.herokuapp.com/"
-HEADLESS = True
+HEADLESS = False
 SLOW_MO = 2000
 VIDEO_WIDTH_SIZE = 1920
 VIDEO_HEIGHT_SIZE = 1080
@@ -7,7 +7,7 @@ VIEW_PORT_WIDTH_SIZE = 1920
 VIEW_PORT_HEIGHT_SIZE = 1080
 
 # DATABASE
-HOST="localhost"
-DBNAME="playwrightdemo"
-USER="postgres"
-PASSWORD="postgres"
+HOST = "localhost"
+DBNAME = "playwrightdemo"
+USER = "postgres"
+PASSWORD = "postgres"
